6c69692ac0bcf1b2966bc61bd8d2788ac11afe9b
all4oneTu <81431586+all4oneTu@users.noreply.github.com>
2022-05-28 22:50:54 +0700

    update

---
+++ .history/src/components/Constant/Constant_20220528224922.js
@@ -0,0 +1,4 @@
+export const urlLink = "https://spring-react-mongodb-back-end.herokuapp.com"
+// export const localhost = "https://spring-react-mongodb-back-end.herokuapp.com"
+export const localhost = "http://localhost:8080"
+export const currentLocation = "http://localhost:3000/"
--- .history/src/components/Constant/Constant_20220528224957.js
+++ .history/src/components/Constant/Constant_20220528224957.js
@@ -4,4 +4,4 @@ export const urlLink = "https://spring-react-mongodb-back-end.herokuapp.com"
 // export const currentLocation = "http://localhost:3000/"
 
 export const localhost = "https://spring-react-mongodb-back-end.herokuapp.com"
-export const currentLocation = "https://react-font-end.vercel.app/"
+export const currentLocation = "https://react-font-end.vercel.app//"
